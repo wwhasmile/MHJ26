103e40890feb0036e654a03d4f2fa639a24dbc0f
wwhasmile <uwuhasmile@proton.me>
2026-03-30 22:07:49 +0300

    Basic movement input implemented

--- Source/MHJ26/Private/Character/MHJPlayerCharacter.cpp
+++ Source/MHJ26/Private/Character/MHJPlayerCharacter.cpp
@@ -2,4 +2,103 @@
 
 
 #include "Character/MHJPlayerCharacter.h"
+#include "Character/MHJCharacterMovementComponent.h"
 
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+AMHJPlayerCharacter::AMHJPlayerCharacter(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	if (UMHJCharacterMovementComponent* MovementComponent = GetCharacterMovement<UMHJCharacterMovementComponent>())
+	{
+		MovementComponent->bCanRun = true;
+	}
+	
+	ViewPitchMin = -89.0f;
+	ViewPitchMax = 80.0f;
+}
+
+void AMHJPlayerCharacter::EnterCinematic()
+{
+	if (APlayerController* PlayerController = CastChecked<APlayerController>(GetController()))
+	{
+		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+		
+		Subsystem->ClearAllMappings();
+		Subsystem->AddMappingContext(CinematicInputMappingContext, 0);
+	}
+}
+
+void AMHJPlayerCharacter::EnterGameplay()
+{
+	if (APlayerController* PlayerController = CastChecked<APlayerController>(GetController()))
+	{
+		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+		
+		APlayerCameraManager* PlayerCameraManager = PlayerController->PlayerCameraManager;
+		const APlayerCameraManager* DefaultPlayerCameraManager = PlayerCameraManager->GetClass()->GetDefaultObject<APlayerCameraManager>();
+		PlayerCameraManager->ViewYawMin = DefaultPlayerCameraManager->ViewYawMin;
+		PlayerCameraManager->ViewYawMax = DefaultPlayerCameraManager->ViewYawMax;
+		PlayerCameraManager->ViewPitchMin = ViewPitchMin;
+		PlayerCameraManager->ViewPitchMax = ViewPitchMax;
+		
+		Subsystem->ClearAllMappings();
+		Subsystem->AddMappingContext(GeneralInputMappingContext, 0);
+	}
+}
+
+void AMHJPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	EnterGameplay();
+	
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AMHJPlayerCharacter::Look);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMHJPlayerCharacter::Move);
+		EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Started, this, &AMHJPlayerCharacter::StartRunning);
+		EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Completed, this, &AMHJPlayerCharacter::StopRunning);
+	}
+}
+
+void AMHJPlayerCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D Axis = Value.Get<FVector2D>();
+	AddControllerPitchInput(Axis.Y);
+	AddControllerYawInput(Axis.X);
+}
+
+void AMHJPlayerCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D Axis = Value.Get<FVector2D>();
+	
+	if (const AController* CharacterController = GetController())
+	{
+		const FRotator ControlRotation = CharacterController->GetControlRotation();
+		const FRotator YawRotation(0.0f, ControlRotation.Yaw, 0.0f);
+	
+		const FVector ForwardVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	
+		AddMovementInput(ForwardVector, Axis.Y);
+		AddMovementInput(RightVector, Axis.X);
+	}
+}
+
+void AMHJPlayerCharacter::StartRunning(const FInputActionValue& Value)
+{
+	if (UMHJCharacterMovementComponent* MovementComponent = GetCharacterMovement<UMHJCharacterMovementComponent>())
+	{
+		MovementComponent->StartRunning();
+	}
+}
+
+void AMHJPlayerCharacter::StopRunning(const FInputActionValue& Value)
+{
+	if (UMHJCharacterMovementComponent* MovementComponent = GetCharacterMovement<UMHJCharacterMovementComponent>())
+	{
+		MovementComponent->StopRunning();
+	}
+}
--- Source/MHJ26/Public/Character/MHJPlayerCharacter.h
+++ Source/MHJ26/Public/Character/MHJPlayerCharacter.h
@@ -6,6 +6,9 @@
 #include "Character/MHJCharacter.h"
 #include "MHJPlayerCharacter.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+
 /**
  * 
  */
@@ -13,5 +16,51 @@ UCLASS()
 class MHJ26_API AMHJPlayerCharacter : public AMHJCharacter
 {
 	GENERATED_BODY()
+	
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input", meta=(AllowPrivateAccess=true))
+	float ViewPitchMin;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input", meta=(AllowPrivateAccess=true))
+	float ViewPitchMax;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input", meta=(AllowPrivateAccess=true))
+	TObjectPtr<UInputMappingContext> GeneralInputMappingContext;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input", meta=(AllowPrivateAccess=true))
+	TObjectPtr<UInputMappingContext> CinematicInputMappingContext;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input", meta=(AllowPrivateAccess=true))
+	TObjectPtr<UInputAction> LookAction;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input", meta=(AllowPrivateAccess=true))
+	TObjectPtr<UInputAction> MoveAction;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input", meta=(AllowPrivateAccess=true))
+	TObjectPtr<UInputAction> RunAction;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input", meta=(AllowPrivateAccess=true))
+	TObjectPtr<UInputAction> InteractAction;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input", meta=(AllowPrivateAccess=true))
+	TObjectPtr<UInputAction> AttackAction;
+	
+	uint8 bInCinematic:1;
+	
+public:
+	AMHJPlayerCharacter(const FObjectInitializer& ObjectInitializer);
+	
+	UFUNCTION(BlueprintCallable, Category="Input")
+	void EnterCinematic();
+	UFUNCTION(BlueprintCallable, Category="Input")
+	void EnterGameplay();
+	
+protected:
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+	
+private:
+	UFUNCTION()
+	void Look(const FInputActionValue& Value);
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
+	
+	UFUNCTION()
+	void StartRunning(const FInputActionValue& Value);
+	UFUNCTION()
+	void StopRunning(const FInputActionValue& Value);
 	
 };
