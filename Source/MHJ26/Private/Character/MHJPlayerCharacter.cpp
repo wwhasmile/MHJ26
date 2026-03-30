83dedb1d1cad26abcbd78c47d3c7e455fc1acffe
wwhasmile <uwuhasmile@proton.me>
2026-03-30 19:05:21 +0300

    Player character class creation

---
+++ Source/MHJ26/Private/Character/MHJPlayerCharacter.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2026 Hasmile
+
+
+#include "Character/MHJPlayerCharacter.h"
+
---
+++ Source/MHJ26/Public/Character/MHJPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2026 Hasmile
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/MHJCharacter.h"
+#include "MHJPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MHJ26_API AMHJPlayerCharacter : public AMHJCharacter
+{
+	GENERATED_BODY()
+	
+};
