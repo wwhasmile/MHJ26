c76b748b57761e5888a73497389f181fadd1ff42
wwhasmile <uwuhasmile@proton.me>
2026-04-07 12:18:51 +0300

    Added empty footstep anim notify

---
+++ Source/MHJ26/Private/Animation/MHJAnimNotify_Footstep.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2026 Hasmile
+
+
+#include "Animation/MHJAnimNotify_Footstep.h"
---
+++ Source/MHJ26/Public/Animation/MHJAnimNotify_Footstep.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2026 Hasmile
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "MHJAnimNotify_Footstep.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MHJ26_API UMHJAnimNotify_Footstep : public UAnimNotify
+{
+	GENERATED_BODY()
+};
